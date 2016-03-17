3fdf49b062576db5e40f2401b300c899bb0b5d82
Duncan <duncan.sanders-fox@mobysoft.com>
2016-03-17 14:15:29 +0000

    Initial Checkin 2

---
+++ PlatformA/src/main/java/com/mobysoft/platform/a/PlatformA.java
@@ -0,0 +1,16 @@
+package com.mobysoft.platform.a;
+
+/**
+ * Created by Duncan on 17/03/2016.
+ */
+public class PlatformA {
+    String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
---
+++ PlatformB/src/main/java/com/mobysoft/platform/b/PlatformB.java
@@ -0,0 +1,7 @@
+package com.mobysoft.platform.b;
+
+/**
+ * Created by Duncan on 17/03/2016.
+ */
+public class PlatformB {
+}
